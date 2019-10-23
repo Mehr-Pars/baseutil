294e6bd33cba16312fbd29ab300b8de4bdac5494
aliarasteh <aliarasteh93@gmail.com>
2019-10-23 15:07:35 +0330

    DateUtils

--- module/src/main/java/epeyk/mobile/baseutil/common/DateUtils.kt
+++ module/src/main/java/epeyk/mobile/baseutil/common/DateUtils.kt
@@ -7,29 +7,56 @@ import java.util.*
 
 object DateUtils {
 
-    fun getPersianDateFromTimeStamp(timeStamp: Long): String {
+    fun getPersianDateFromTimeStamp(timeStamp: Long, showTime: Boolean = false): String {
         val timeMillis = timeStamp * 1000
-        return getPersianDate(timeMillis)
+        return getPersianDate(timeMillis, showTime)
     }
 
-    fun getPersianDate(timeMillis: Long): String {
+    fun getTimeStampFromPersianDate(stringDate: String): Long {
+        try {
+            val array =
+                if (stringDate.contains("-")) stringDate.split("-".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray() else stringDate.split(
+                    "/".toRegex()
+                ).dropLastWhile { it.isEmpty() }.toTypedArray()
+            val calendarTool = CalendarTool()
+            calendarTool.setIranianDate(
+                Integer.parseInt(array[0]),
+                Integer.parseInt(array[1]),
+                Integer.parseInt(array[2])
+            )
+            val formatter = SimpleDateFormat("yyyy/MM/dd", Locale("en"))
+            val date = formatter.parse(calendarTool.gregorianDate) as Date
+            return date.time / 1000
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        return 0
+    }
+
+    fun getPersianDate(timeMillis: Long, showTime: Boolean = false): String {
         val date = Date(timeMillis)
         val calendar = ShamsiCalendar()
         val solarCalendar = calendar.SolarCalendar(date)
 
-        val t = String.format("%2d:%2d:%2d", date.hours, date.minutes, date.seconds)
-        return "${solarCalendar.year}-${solarCalendar.month}-${solarCalendar.date}_$t"
+        val time = if (showTime) String.format(
+            " - %02d:%02d:%02d",
+            date.hours,
+            date.minutes,
+            date.seconds
+        ) else ""
+        return "%d/%d/%d$time".format(solarCalendar.year, solarCalendar.month, solarCalendar.date)
     }
 
     fun getPersianDate(stringDate: String): String? {
-        val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale("en"))
+        val sdf = SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale("en"))
         try {
             val date = sdf.parse(stringDate.replace("T", " "))
 
             val calendar = ShamsiCalendar()
             val solarCalendar = calendar.SolarCalendar(date)
 
-            return "${solarCalendar.year}-${solarCalendar.month}-${solarCalendar.date}"
+            return "${solarCalendar.year}/${solarCalendar.month}/${solarCalendar.date}"
         } catch (e: Exception) {
             e.printStackTrace()
         }
@@ -38,7 +65,7 @@ object DateUtils {
     }
 
     fun getPersianDateComplete(stringDate: String): String? {
-        val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale("en"))
+        val sdf = SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale("en"))
         sdf.timeZone = TimeZone.getTimeZone("UTC")
         try {
             val date = sdf.parse(stringDate.replace("T", " "))
@@ -47,7 +74,7 @@ object DateUtils {
             val solarCalendar = calendar.SolarCalendar(date)
 
             return String.format(
-                Locale.US, "%02d:%02d , %d-%02d-%02d", date.hours, date.minutes,
+                Locale.US, "%02d:%02d , %d/%02d/%02d", date.hours, date.minutes,
                 solarCalendar.year, solarCalendar.month, solarCalendar.date
             )
         } catch (e: Exception) {
